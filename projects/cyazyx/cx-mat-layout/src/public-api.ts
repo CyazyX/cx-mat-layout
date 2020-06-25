eb4db9d40b4d161c804feffcafd9d543993c41de
Cyrus Mushier <mushierc@gmail.com>
2020-06-25 11:32:46 +0300

    Release v1.0.0

--- projects/cyazyx/cx-mat-layout/src/public-api.ts
+++ projects/cyazyx/cx-mat-layout/src/public-api.ts
@@ -4,6 +4,6 @@
 
 export * from './lib/models';
 export * from './lib/services';
-export * from './lib/directives';
+// export * from './lib/directives'; // NOT yet defined
 export * from './lib/components';
 export * from './lib/cx-mat-layout.module';
